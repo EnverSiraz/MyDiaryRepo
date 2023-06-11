2954eba860e32a91da8cefa972f11768b5078a74
EnverSiraz <130348232+EnverSiraz@users.noreply.github.com>
2023-06-11 20:23:11 +0300

    pazar
    ilk build

--- DiaryJS/index.js
+++ DiaryJS/index.js
@@ -4,23 +4,26 @@ function GetAll() {
             let data = res.data
             document.getElementById("tabloBody").innerHTML = ""
 
-            data.forEach(element => {
+            data.reverse().forEach(element => {
                 console.log(element);
                 let trElement = document.createElement("tr")
                 let thElement1 = document.createElement("th")
                 let thElement2 = document.createElement("th")
                 let thElement3 = document.createElement("th")
 
-                
+
 
                 thElement1.innerHTML = element.title
-                thElement2.innerHTML = element.body 
-                
-                
-                thElement3.innerHTML = `<button onclick="DeleteData(${element.id})" >Delete</button>`;
+                thElement2.innerHTML = element.body
+
+
+                thElement3.innerHTML = `<div id="buttonlarDiv">
+                <button onclick="Update(${element.id})" id="buttonlarUpdate" ><i class="fas fa-wrench fa-xl"></i></button> <button onclick="DeleteData(${element.id})" id="buttonlarDelete" ><i class="far fa-trash-alt fa-xl"></i></button>
+              
+              </div>`;
                 console.log(thElement3)
 
-                trElement.appendChild(thElement1)   
+                trElement.appendChild(thElement1)
                 trElement.appendChild(thElement2)
                 trElement.appendChild(thElement3)
                 document.getElementById("tabloBody").appendChild(trElement)
@@ -29,61 +32,80 @@ function GetAll() {
         })
 }
 
-
-// function addNew(){
-//     let title = document.getElementById("titlebox").value
-//     let text = document.getElementById("textbox").value
-
-//     let newDiary = {
-//         companyName:title,
-//         contactName :text
-//     }
-//     axios.post("https://northwind.vercel.app/api/categories",newDiary)
-//     .then (res => {
-//         console.log(res)
-//         alert("Success")
-//         GetAll()
-
-//     })}
+// thElement3.innerHTML = `<button onclick="Update(${element.id})" id="buttonlarUpdate" >Update</button> <button onclick="DeleteData(${element.id})" id="buttonlarDelete" ><i class="far fa-trash-alt fa-xl"></i></button>`;
+// console.log(thElement3)
 
 
 
 
-function add() {
+function create() {
 
-    let title = document.getElementById('titlebox').value;
-    let body = document.getElementById('textbox').value
+    let title = document.getElementById('titlebox').value.trim();
+    let body = document.getElementById('textbox').value.trim()
 
-    let newDiary = {
-        title : title,
-        body : body
+    if (title !="" && body!="") {
+       
+        let newDiary = {
+            title: title,
+            body: body
+        }
+    
+    
+        axios.post('https://localhost:7224/api/Diary/create', newDiary)
+            .then(res => {
+                console.log('Response', res);
+                document.getElementById("tabloBody").innerHTML = ""
+                GetAll()
+                alert('New note Succesfully added!')
+            })
+
     }
 
+    if(title =="" && body!=""){alert("Title is empty!! ")}
+    if(title !="" && body==""){alert("Text is empty!!")}
+   
+    
 
-    axios.post('https://localhost:7224/api/Diary', newDiary)
-        .then(res => {
-            console.log('Response', res);
-            alert('Ekleme işlemi başarılı')
-        })
+
 
 }
 
 
 
 
-// function DeleteData(id) {
-    
-//     axios.delete('https://localhost:7224/api/Diary/'+id)
-//     .then(res => {
-//         alert("Success")
-//         document.getElementById("tabloBody").innerHTML=""
-//         GetAll()
+function DeleteData(id) {
+
+    axios.delete('https://localhost:7224/api/Diary/delete/' + id)
+        .then(res => {
+            alert("Success")
+            document.getElementById("tabloBody").innerHTML = ""
+            GetAll()
 
 
 
-//     })
+        })
 
-// }
+}
+
+function Update(id) {
+
+    let newTitle = document.getElementById('titlebox').value;
+    let newBody = document.getElementById('textbox').value;
+
+
+    axios.put("https://localhost:7224/api/Diary/update/" + id, { title: newTitle, body: newBody })
+        .then(res => {
+            alert("Success")
+            document.getElementById("tabloBody").innerHTML = ""
+            GetAll()
+
+
+        })
+
+
+
+
+}
 
 
 GetAll()
